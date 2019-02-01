02230ce1f47992673e616dac4bb01c3799e74bb1
Floris <Flomo123456789@gmail.com>
2019-02-01 15:28:46 +0100

    commit jippy

---
+++ CameraZoom.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour {
+
+    private float timer;
+    private float duration;
+    private bool isActive;
+
+    public void Zoom(float m_duration)
+    {
+        duration = m_duration;
+        isActive = true;
+    }
+
+    void Update()
+    {
+        if (isActive == true)
+        {
+            print("dab");
+            transform.rotation = new Quaternion(0, 0, -0.05f, 1);
+            GetComponent<Camera>().orthographicSize = 4.0f;
+            timer += Time.deltaTime;
+            if (timer > duration)
+            {
+                duration = 0;
+                timer = 0;
+                isActive = false;
+            }
+        }
+        else
+        {
+            GetComponent<Camera>().orthographicSize = 5.0f;
+            transform.rotation = new Quaternion(0,0,0,1);
+        }
+    }
+}
---
+++ ChampionChangeForm.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChampionChangeForm : MonoBehaviour {
+
+    private bool inForm1 = true;
+
+    private SpriteRenderer spriteR;
+
+    [Header("Sprites")]
+    [SerializeField]
+    private Sprite Form1;
+    [SerializeField]
+    private Sprite Form2;
+
+    void Start()
+    {
+        spriteR = gameObject.GetComponent<SpriteRenderer>();
+
+        GameObject knop = GameObject.Find("FormChanger");
+        Button buttonComp = knop.GetComponent<Button>();
+
+        buttonComp.onClick.AddListener(ChangeForm);
+    }
+
+    void Update()
+    {
+        if (inForm1 == true)
+        {
+            spriteR.sprite = Form1;
+        }
+        else
+        {
+            spriteR.sprite = Form2;
+        }
+    }
+
+    public void ChangeForm()
+    {
+        GameObject.Find("Ability1").GetComponent<AbilityChange>().changeForm();
+        GameObject.Find("Ability2").GetComponent<AbilityChange>().changeForm();
+        GameObject.Find("Ability3").GetComponent<AbilityChange>().changeForm();
+        GameObject.Find("Ability4").GetComponent<AbilityChange>().changeForm();
+        GameObject.Find("icon").GetComponent<AbilityChange>().changeForm();
+        inForm1 = !inForm1;
+    }
+}
